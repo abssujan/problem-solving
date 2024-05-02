7289fd98ab153209e791cbc2d5c309f4ba9af3e1
absujan <abdussabursujan@gmail.com>
2024-05-03 00:45:14 +0600

    2nd provlem solved

--- problem.js
+++ problem.js
@@ -1,3 +1,5 @@
+//1.
+
 //If we list all the natural numbers below 10 that are multiples of 3 or 5,
 // we get 3, 5, 6 and 9. The sum of these multiples is 23. Finish the solution
 //so that it returns the sum of all the multiples of 3 or 5 below the number passed in.
@@ -11,5 +13,28 @@ const solution = number => {
     }
     console.log(sum)
 }
+//solution(10)
+// *************************************************************************************
 
-solution(10)
+// 2.
+// Create a function that takes an integer as an
+// argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+function oddAndEvent(number){
+    if( number % 2 === 0){
+        console.log('Even')
+    }else{
+        console.log('Odd')
+    }
+}
+//oddAndEvent(8)
+
+// shortCut way 
+const odd_or_even = number => {
+    //ternary operator
+    return number % 2 === 0 ? 'Even' : 'Odd';
+}
+//const result = odd_or_even(10);
+//console.log(result);
+
+// *************************************************************************************
